1a9330ad173c42c2ae5fb89565a9f0e164549951
Svante Geisshirt <svante2001@gmail.com>
2019-02-27 20:51:57 +0100

    2 sensor linefollower for the micro:bot

--- main.ts
+++ main.ts
@@ -1,22 +1,33 @@
-//This is a robot linefollowing code with one sensor
-
 import kitronik = kitronik_motor_driver
 
+let Right_Detector = 0
 let Left_Detector = 0
+let black = 0
+let white = 1
+
 
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 
-//0 is the black line
-//1 is the white background
+// 0 is the black line
+// 1 is the white background
+// motor1 is the left 3V DC motor
+// motor2 is he right 3V DC motor
 
 basic.forever(function () {
-    Left_Detector = pins.digitalReadPin(DigitalPin.P1) 
-     if (Left_Detector == 1) {
-        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Reverse, 40)
-        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Forward, 10)
-    } else if (Left_Detector = 0) {
-        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Reverse, 40)
-        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Forward, 10)
+    Right_Detector = pins.digitalReadPin(DigitalPin.P2)
+    Left_Detector = pins.digitalReadPin(DigitalPin.P1)
+    if (Right_Detector == white && Left_Detector == black) {
+        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Reverse, 30)
+        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Reverse, 30)
+    } else if (Right_Detector == black && Left_Detector == black) {
+        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Reverse, 30)
+        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Forward, 15)
+    } else if (Right_Detector == black && Left_Detector == white) {
+        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Reverse, 30)
+        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Forward, 15)
+    } else if (Right_Detector == white && Left_Detector == white) {
+        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Forward, 15)
+        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Reverse, 30)
     }
 })

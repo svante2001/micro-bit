080c74e6fb841d6fc110c60772e9e6b061094265
Svante Geisshirt <svante2001@gmail.com>
2019-02-27 20:27:20 +0100

    clean up

--- main.ts
+++ main.ts
@@ -6,54 +6,8 @@ let Right_Detector = 0
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 
-
-function runForMS(motor1: number, motor2: number, ms: number) {
-    let running = true;
-    control.inBackground(() => {
-        if (motor1 === 0) {
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-        } else if (motor1 < 0) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1,
-                kitronik_motor_driver.MotorDirection.Reverse,
-                Math.abs(motor1))
-        } else {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1,
-                kitronik_motor_driver.MotorDirection.Forward,
-                motor1);
-        }
-        if (motor2 === 0) {
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-        } else if (motor2 < 0) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2,
-                kitronik_motor_driver.MotorDirection.Reverse,
-                Math.abs(motor2))
-        } else {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2,
-                kitronik_motor_driver.MotorDirection.Forward,
-                motor2)
-        }
-        while (running) {
-            basic.pause(1)
-        }
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-    })
-    basic.pause(ms)
-    running = false
-}
-
-// runForMS(10, 10, 10)
-// runForMS(0, 0, 10)
-// runForMS(-10, -10, 10)
-// runForMS(0, 0, 10)
-// venstre, højre, tid
-
-input.onButtonPressed(Button.A, function () {
-    runForMS(30, 30, 1000)
-})
-
-//0 er sort
-//1 er hvid
+//0 is black line
+//1 is white line
 
 basic.forever(function () {
     Left_Detector = pins.digitalReadPin(DigitalPin.P1) 

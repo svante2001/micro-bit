17f9b365639e55df7b67d5b4b43181f1f84d244a
Svante Geisshirt <svante2001@gmail.com>
2019-03-08 10:18:17 +0100

    3rd commit

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 
  function runForMS(motor1: number, motor2: number, ms: number) {
-    let running = true;
+    let running = true
     control.inBackground(() => {
         if (motor1 === 0) {
             kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
@@ -20,7 +20,7 @@ pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
         } else {
             kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1,
                 kitronik_motor_driver.MotorDirection.Forward,
-                motor1);
+                motor1)
         }
         if (motor2 === 0) {
             kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
@@ -36,7 +36,7 @@ pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
         while (running) {
             basic.pause(1)
         }
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1) 
         kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
     })
     basic.pause(ms)
@@ -46,7 +46,7 @@ pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 //0 is the black line
 //1 is the white background
  
-// testing of showNumber with sensor
+// testing of showNumber with sensor to calibrate. Out comment line 62-84
 
 basic.forever(function() {
     Right_Detector = pins.digitalReadPin(DigitalPin.P2) 
@@ -61,26 +61,24 @@ basic.forever(function() {
 
 // Count lines while following main line
 
- basic.forever(function() {
+basic.forever(function() {
     Left_Detector = pins.digitalReadPin(DigitalPin.P1) 
     Right_Detector = pins.digitalReadPin(DigitalPin.P2) 
-    if (Right_Detector == 1 && Left_Detector == 1){
+    if (Left_Detector == 1) {
         kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Reverse, 40)
         kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Forward, 10)
         basic.showNumber(count)
-    } else if (Right_Detector == 1 && Left_Detector == 0){
+    } else if (Left_Detector == 0) {
         kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Reverse, 40)
         kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Forward, 10)
         basic.showNumber(count)
-    } else if (Right_Detector == 0 && Left_Detector == 1){
-        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Reverse, 40)
-        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Forward, 10)
+    } else if (Left_Detector == 1) {
+        runForMS(-40, -40, 2000)
         count ++
         basic.showNumber(count)
-    } else if (Right_Detector == 0 && Left_Detector == 0){
-        kitronik.motorOn(kitronik.Motors.Motor2, kitronik.MotorDirection.Reverse, 40)
-        kitronik.motorOn(kitronik.Motors.Motor1, kitronik.MotorDirection.Forward, 10)
-        count ++
-        basic.showNumber(count)
+    } if (count == 2) { //how many lines to drive past
+        runForMS(20, -20, 2000) //turn off the main line
+        kitronik.motorOff(kitronik.Motors.Motor1)
+        kitronik.motorOff(kitronik.Motors.Motor2)        
     }
- })
+})
